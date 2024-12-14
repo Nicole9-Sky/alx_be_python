8cd2e55031e986c47d25e6484422077c2ee8c4e9
Nicole9-Sky <cezeh247@gamil.com>
2024-12-15 00:31:49 +0100

    calculating the user’s monthly savings based on inputted monthly income and expenses

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = float(input("Enter your monthly income"))
+monthly_expenses = float(input("Enter your monthly expenses"))
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"your monthly_savings are $")
+print(f"projected savings after one year, with interest, is: $ {annual_savings: 2f}.")
