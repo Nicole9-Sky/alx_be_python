7d4db797d35338cfcadd7998dd56db2e16a50613
Nicole9-Sky <cezeh247@gamil.com>
2024-12-21 15:33:55 +0100

    calculator will prompt the user to enter two numbers and select an operation (addition, subtraction, multiplication, or division)

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,28 +1,28 @@
 num1 = float(input("Enter the first number: ")) 
 num2 = float(input("Enter the second number: "))
-operation = input("Choose the operation (+, -, *, /)").strip()
+operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
     
     case "+":
         result = num1 + num2
-        print(f"the result is {result}. ")
+        print(f"the result is {result}.")
         
     case "-":
             result =  num1 - num2
-            print(f"the result is {result}. ")
+            print(f"the result is {result}.")
             
     case "*":
             result = num1 * num2
-            print(f"the result is {result}. ")
+            print(f"the result is {result}.")
             
     case "/":
         if num2 != 0:
             result = num1 / num2
-            print(f"the result is {result}. ")
+            print(f"the result is {result}.")
         else:
             print("cannot divide by zero.")
          
     case _:
-            print("Invalid operation. ")
+            print("Invalid operation.")
          
