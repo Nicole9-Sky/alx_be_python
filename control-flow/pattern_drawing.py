b885c98149a7bf30c67791ff30410bc6af6a749e
Nicole9-Sky <cezeh247@gamil.com>
2024-12-23 00:10:41 +0100

    prompt the user to enter a positive integer, then use nested loops to print a square pattern of that size made of asterisks

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,10 @@
 #Prompt the user for the size of the pattern
-size = int(input("Enter the size of the patter: "))
-#Initialize the row counter
-row = 0
+size = int(input("Enter the size of the pattern:"))
 #Use a while loop to iterate through each row
+row = 0
 while row < size:
     #use a for loop to print asterisks in the row
     for _ in range(size):
-        print("*", end="") #Print asterisks side by side
-        print() #Move to the next line after each row
-        row += 1
+        print("*" * size) # Print asterisks side by side without a new line
+        print()  # Move to the next line after printing a row
+        row +=1 # Increment the row counter
