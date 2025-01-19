0feab4faf6cb8ad0c6539c888742911d43c5d47f
Nicole9-Sky <cezeh247@gamil.com>
2025-01-19 20:39:19 +0100

    Create a test_simple_calculator.py script to define and run unit tests for each method in the SimpleCalculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,37 +8,37 @@ class TestSimpleCalculator(unittest.TestCase):
         
     def test_addition(self):
         """Test the addition method."""
-        self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(2, 2), 4)
-        self.assertEqual(self.calc.add(3, 2), 5)
+        self.assertEqual(self.calc.add(2, 3),5)
+        self.assertEqual(self.calc.add(-1, 1),0)
+        self.assertEqual(self.calc.add(2, 2),4)
+        self.assertEqual(self.calc.add(3, 2),5)
         
         # Add more assertions to thoroughly test the add method.
         
     def tes_subtraction(self):
         """Test the subtraction method.""" 
-        self.assertEqual(self.calc.subtract(5, 3), 2)   
-        self.assertEqual(self.calc.subtract(1, 1), 0)   
-        self.assertEqual(self.calc.subtract(-5, 3), -8)   
-        self.assertEqual(self.calc.subtract(6, 2), 4)  
+        self.assertEqual(self.calc.subtract(5, 3),2)   
+        self.assertEqual(self.calc.subtract(1, 1),0)   
+        self.assertEqual(self.calc.subtract(-5, -3),-2)   
+        self.assertEqual(self.calc.subtract(6, 2),4)  
         
     def test_multiplication(self):
         """test_multiplication method."""
-        self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(4, 1), 4)
-        self.assertEqual(self.calc.multiply(2, 2), 4)
-        self.assertEqual(self.calc.multiply(1, 0), 0)
+        self.assertEqual(self.calc.multiply(2, 3),6)
+        self.assertEqual(self.calc.multiply(4, 1),4)
+        self.assertEqual(self.calc.multiply(2, 2),4)
+        self.assertEqual(self.calc.multiply(1, 0),0)
         
     def test_division(self):
         """test_division method."""
-        self.assertEqual(self.calc.divide(6, 2), 3)    
-        self.assertEqual(self.calc.divide(4, 2), 2)    
-        self.assertEqual(self.calc.divide(-6, 2), -3)    
-        self.assertEqual(self.calc.divide(7.5, 2.5), 3.0)   
+        self.assertEqual(self.calc.divide(6, 2),3)    
+        self.assertEqual(self.calc.divide(4, 2),2)    
+        self.assertEqual(self.calc.divide(-6, 2),-3)    
+        self.assertEqual(self.calc.divide(7.5, 2.5),3.0)   
         
         
-        self.assertIsNone(self.calc.divide(5, 0), 0) 
-        self.assertIsNone(self.calc.divide(0, 0), 0) 
+        self.assertIsNone(self.calc.divide(5, 0),0) 
+        self.assertIsNone(self.calc.divide(0, 0),0) 
         
         if __name__ == "__main__":
             unittest.main()
