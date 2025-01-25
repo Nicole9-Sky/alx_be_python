5ac89ba6272cf26b2f5b1a985616ca5ded92d828
Nicole9-Sky <cezeh247@gamil.com>
2025-01-25 15:00:04 +0100

    Additionally, implement a Library class demonstrating composition by managing a collection of books

--- oop/library_system.py
+++ oop/library_system.py
@@ -21,7 +21,7 @@ class PrintBook(Book):
         self.page_count = page_count
         
     def __str__(self):
-            return f"PrintBook: {self.title} by {self.author}, Page_Count: {self.page_count}KB"
+            return f"PrintBook: {self.title} by {self.author}, Page_Count: {self.page_count}"
 class Library:
     
     def __init__(self):
